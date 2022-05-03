4797b069384fb029611f194eb83f620a823bef5b
lewis george <ekoboekobo22@gmail.com>
2022-05-02 20:29:24 -0400

    Boa 2nd deployment created

--- deploy.py
+++ deploy.py
@@ -1,4 +1,7 @@
 Deploying our python applications
 client: BoA
 this is our top client for 2022
-please use this for deployment 
+please use this for deployment
+My Lord Jesus the Son of God will see me through my trials and tribulations
+I will beat my case and will also go through my DevOps study guide and land myself multiple jobs
+IN JESUS NAME AMEN
